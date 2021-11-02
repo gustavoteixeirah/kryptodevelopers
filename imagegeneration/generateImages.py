b1f262dad46792ca9a73224d80a5e8c7aa6ddf1a
Gustavo Teixeira <iwhrim@gmail.com>
2021-11-02 18:42:30 -0300

    atualizado traits

--- imagegeneration/generateImages.py
+++ imagegeneration/generateImages.py
@@ -11,61 +11,61 @@ print(path)
 with open("./traits.json", 'r') as f:
         traits = json.load(f)
         
-#### IMAGE GENERATION
-for trait in traits:
-    # BACKGROUND
-    background = Image.open(f'{path}/images/background/{trait["Background"]}.png').convert('RGBA')
+# #### IMAGE GENERATION
+# for trait in traits:
+#     # BACKGROUND
+#     background = Image.open(f'{path}/images/background/{trait["Background"]}.png').convert('RGBA')
     
-    # DEVELOPER
-    developer = Image.open(f'{path}/images/developer/{trait["Developer"]}.png').convert('RGBA')
+#     # DEVELOPER
+#     developer = Image.open(f'{path}/images/developer/{trait["Developer"]}.png').convert('RGBA')
 
-    # HAIR
-    if (trait["Accessory"] == "Milos"):
-        hair = Image.open(f'{path}/images/hair_male/None.png').convert('RGBA')
-    else:
-        if (trait["Developer"] == "Jack"):
-            hair = Image.open(f'{path}/images/hair_male/{trait["Hair"]}.png').convert('RGBA')
-        else:
-            hair = Image.open(f'{path}/images/hair_female/{trait["Hair"]}.png').convert('RGBA')
+#     # HAIR
+#     if (trait["Accessory"] == "Milos"):
+#         hair = Image.open(f'{path}/images/hair_male/None.png').convert('RGBA')
+#     else:
+#         if (trait["Developer"] == "Jack"):
+#             hair = Image.open(f'{path}/images/hair_male/{trait["Hair"]}.png').convert('RGBA')
+#         else:
+#             hair = Image.open(f'{path}/images/hair_female/{trait["Hair"]}.png').convert('RGBA')
 
-    # ACCESSORY
-    accessory = Image.open(f'{path}/images/accessory/{trait["Accessory"]}.png').convert('RGBA')
+#     # ACCESSORY
+#     accessory = Image.open(f'{path}/images/accessory/{trait["Accessory"]}.png').convert('RGBA')
     
-    # PAINTING
-    if (trait["Background"] == "Company"):
-        painting = Image.open(f'{path}/images/Painting_Company/{trait["Painting"]}.png').convert('RGBA')
-    elif (trait["Background"] == "Kitchen"):
-        painting = Image.open(f'{path}/images/Painting_Kitchen/{trait["Painting"]}.png').convert('RGBA')
-    elif (trait["Background"] == "Tower"):
-        painting = Image.open(f'{path}/images/Painting_Tower/{trait["Painting"]}.png').convert('RGBA')
-    else:
-        painting = Image.open(f'{path}/images/painting/{trait["Painting"]}.png').convert('RGBA')
+#     # PAINTING
+#     if (trait["Background"] == "Company"):
+#         painting = Image.open(f'{path}/images/Painting_Company/{trait["Painting"]}.png').convert('RGBA')
+#     elif (trait["Background"] == "Kitchen"):
+#         painting = Image.open(f'{path}/images/Painting_Kitchen/{trait["Painting"]}.png').convert('RGBA')
+#     elif (trait["Background"] == "Tower"):
+#         painting = Image.open(f'{path}/images/Painting_Tower/{trait["Painting"]}.png').convert('RGBA')
+#     else:
+#         painting = Image.open(f'{path}/images/painting/{trait["Painting"]}.png').convert('RGBA')
     
-    # BEVERAGE
-    beverage = Image.open(f'{path}/images/beverage/{trait["Beverage"]}.png').convert('RGBA')
+#     # BEVERAGE
+#     beverage = Image.open(f'{path}/images/beverage/{trait["Beverage"]}.png').convert('RGBA')
 
-    #Create each composite
-    com1 = Image.alpha_composite(background, painting)
-    com2 = Image.alpha_composite(com1, developer)
-    com3 = Image.alpha_composite(com2, hair)
-    com4 = Image.alpha_composite(com3, accessory)
-    com5 = Image.alpha_composite(com4, beverage)
+#     #Create each composite
+#     com1 = Image.alpha_composite(background, painting)
+#     com2 = Image.alpha_composite(com1, developer)
+#     com3 = Image.alpha_composite(com2, hair)
+#     com4 = Image.alpha_composite(com3, accessory)
+#     com5 = Image.alpha_composite(com4, beverage)
 
-    #Convert to RGB
-    rgb_im = com5.convert('RGB')
-    resizedImage = rgb_im.resize((512,512), Image.NEAREST)
+#     #Convert to RGB
+#     rgb_im = com5.convert('RGB')
+#     resizedImage = rgb_im.resize((512,512), Image.NEAREST)
 
-    file_name = str(trait["tokenId"]) + ".png"
-    resizedImage.save("./output/" + file_name)
-    print(f'{str(trait["tokenId"])} done')  
+#     file_name = str(trait["tokenId"]) + ".png"
+#     resizedImage.save("./output/" + file_name)
+#     print(f'{str(trait["tokenId"])} done')  
 
 
-# with open("imagegeneration/hashes2.json", 'r') as f:
-#     hashes = json.load(f)
+with open("imagegeneration/hashes2.json", 'r') as f:
+    hashes = json.load(f)
     
-# for k,v in hashes.items():
-#     print(k,v)
-#     traits[v]["imageIPFS"] = k
+for k,v in hashes.items():
+    print(k,v)
+    traits[v]["imageIPFS"] = k
 
-# with open('traits.json', 'w') as outfile:
-#     json.dump(traits, outfile, indent=4)
+with open('traits.json', 'w') as outfile:
+    json.dump(traits, outfile, indent=4)
