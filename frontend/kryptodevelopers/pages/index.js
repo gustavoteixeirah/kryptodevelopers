700e60f0230ee81cc3dc802b573d439139de4e05
Gustavo Teixeira <iwhrim@gmail.com>
2021-11-16 16:38:57 -0300

    Testing vercel envs

--- frontend/kryptodevelopers/pages/index.js
+++ frontend/kryptodevelopers/pages/index.js
@@ -1,18 +1,25 @@
 import Base from '../components/Base';
-import NavBar from '../components/NavBar';
-import Title from '../components/Title';
 import Description from '../components/Description';
-import Roadmap from '../components/Roadmap';
+import NavBar from '../components/NavBar';
 import Team from '../components/Team';
+import Title from '../components/Title';
 
 export default function Home() {
-  return (
-    <Base>
-      <NavBar />
-      <Title />
-      <Description />
-      {/* <Roadmap /> */}
-      <Team />
-    </Base>
-  );
+    return (
+        <Base>
+            <NavBar />
+            <Title />
+            <Description />
+            {/* <Roadmap /> */}
+            <Team />
+            {process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production' && (
+                <div className="text-xs">
+                    Vercel ENV: {process.env.NEXT_PUBLIC_VERCEL_ENV}
+                    Vercel REF: {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}
+                    Commit version:{' '}
+                    {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}
+                </div>
+            )}
+        </Base>
+    );
 }
