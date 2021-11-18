61ec4a3b2e4fdfd9221695500121472ba1cb9024
Gustavo Teixeira <iwhrim@gmail.com>
2021-11-18 02:10:10 -0300

    Update KryptoDevelopers.sol

--- contract/contracts/KryptoDevelopers.sol
+++ contract/contracts/KryptoDevelopers.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 contract KryptoDevelopers is ERC721Enumerable, Ownable {
     using SafeMath for uint256;
     using Strings for uint256;
+    using Address for address;
 
     mapping(string => bool) _developerExists;
 
@@ -17,18 +19,25 @@ contract KryptoDevelopers is ERC721Enumerable, Ownable {
 
     bool public saleIsActive = false;
 
+    bool public whitelistSaleIsActive = false;
+
     uint256 public constant MAX_MINT_QUANTITY = 25;
 
     uint256 public constant MAX_DEVELOPERS = 10000;
 
+    uint256 public constant WHITELIST_MAX_DEVELOPERS = 3000;
+
     // uint256 public constant developerPrice = 75000000000000000000; // 75 MATIC
     uint256 public constant developerPrice = 1000000000000000000; // 1 MATIC
 
-    // Withdraw addresses
-    // ETB Project
-    address t1 = 0x7393a26c66e6b82944aad564044dc8ed28f786b0;
-    // KryptoDevelopers Project, to use for community events
-    address t2 = xxxxxxxxx;
+    // WHITELIST
+    mapping(address => bool) _whiteListAllowed; //+1
+
+    // WITHDRAW ADDRESSES
+    // ETB Community funds
+    address t1 = 0x7393A26c66E6b82944AaD564044dc8Ed28f786b0;
+    // KryptoDevelopers Community funds
+    address t2 = 0x7393A26c66E6b82944AaD564044dc8Ed28f786b0;
     // Gustavo
     address t3 = 0x3106B9112E18bcB0eBacaE0F9f69aa3A2F1fc9Bd;
     // Renan
@@ -36,34 +45,68 @@ contract KryptoDevelopers is ERC721Enumerable, Ownable {
     // Marcelo
     address t5 = 0x470f4e0314E6f3D0ff52de741fFa5d4Ba93762Af;
 
-    constructor() ERC721("KryptoDevelopers", "KDEV") {
-        developersBaseURI = "https://kryptodevelopers.dev/api/";
-    }
+    constructor() ERC721("KryptoDevelopers", "KDEV") {}
 
-    function flipSaleState() public onlyOwner {
-        saleIsActive = !saleIsActive;
-    }
-
-    // TODO Add logic to mint to whitelist first
     function mint(uint256 quantity) public payable {
-        require(saleIsActive, "Sale must be active to mint Developers");
+        require(
+            saleIsActive || whitelistSaleIsActive,
+            "Sale must be active to mint Developers"
+        );
         require(
             quantity > 0 && quantity <= MAX_MINT_QUANTITY,
             "Can only mint 25 tokens at a time"
         );
         require(
-            totalSupply().add(quantity) <= MAX_DEVELOPERS,
-            "Purchase would exceed max supply of Developers"
+            msg.value >= developerPrice.mul(quantity),
+            "Matic value sent is not correct"
         );
         require(
-            msg.value >= developerPrice.mul(quantity),
-            "Ether value sent is not correct"
+            totalSupply().add(quantity) <
+                (isWhitelistSale() ? WHITELIST_MAX_DEVELOPERS : MAX_DEVELOPERS),
+            "Purchase would exceed max supply of Developers"
         );
+        if (isWhitelistSale()) {
+            require(
+                _whiteListAllowed[msg.sender] == true,
+                "Sender of the transaction is not in the whitelist"
+            );
+        }
         for (uint256 i = 0; i < quantity; i++) {
             uint256 mintIndex = totalSupply();
             if (totalSupply() < MAX_DEVELOPERS) {
                 _safeMint(msg.sender, mintIndex);
             }
+        }
+    }
+
+    function isWhitelistSale() private view returns (bool) {
+        return whitelistSaleIsActive;
+    }
+
+    function addToWhitelist(address[] memory addresses) public onlyOwner {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            address currentAddress = addresses[i];
+
+            require(currentAddress != address(0), "Can't add the null address");
+
+            _whiteListAllowed[currentAddress] = true;
+        }
+    }
+
+    function isWhitelisted(address addr) public view returns (bool) {
+        return _whiteListAllowed[addr];
+    }
+
+    function removeFromAllowList(address[] calldata addresses)
+        public
+        onlyOwner
+    {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            address currentAddress = addresses[i];
+
+            require(currentAddress != address(0), "Can't add the null address");
+
+            _whiteListAllowed[currentAddress] = false;
         }
     }
 
@@ -93,6 +136,14 @@ contract KryptoDevelopers is ERC721Enumerable, Ownable {
         }
     }
 
+    function flipSaleState() public onlyOwner {
+        saleIsActive = !saleIsActive;
+    }
+
+    function flipWhitelistSaleState() public onlyOwner {
+        whitelistSaleIsActive = !whitelistSaleIsActive;
+    }
+
     function setBaseURI(string memory baseURI_) external onlyOwner {
         developersBaseURI = baseURI_;
     }
@@ -109,6 +160,11 @@ contract KryptoDevelopers is ERC721Enumerable, Ownable {
             "ERC721Metadata: URI query for nonexistent token"
         );
         string memory base = developersBaseURI;
+
+        if (bytes(base).length == 0) {
+            return "ipfs://QmSWEwRXCKWcXfw89zTj8CzWbguBQkwVemdVXANeXZZCgw";
+        }
+
         return string(abi.encodePacked(base, tokenId.toString()));
     }
 
@@ -125,23 +181,12 @@ contract KryptoDevelopers is ERC721Enumerable, Ownable {
         return ids;
     }
 
-    function withdrawAll() public payable onlyOwner {
+    function withdrawAll() public onlyOwner {
         uint256 _each = address(this).balance / 5;
         require(payable(t1).send(_each));
         require(payable(t2).send(_each));
         require(payable(t3).send(_each));
         require(payable(t4).send(_each));
         require(payable(t5).send(_each));
-    }
-
-    function giveAway(address _to, uint256 _amount) external onlyOwner {
-        require(_amount <= _reserved, "Exceeds reserved developer supply");
-
-        uint256 supply = totalSupply();
-        for (uint256 i; i < _amount; i++) {
-            _safeMint(_to, supply + i);
-        }
-
-        _reserved -= _amount;
     }
 }
